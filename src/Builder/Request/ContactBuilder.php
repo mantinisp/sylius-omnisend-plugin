03e45d9d4bd70b5db44ba9838612487b7c861025
mantaspudziavis <mantas.p@nordcode.io>
2021-08-10 16:34:17 +0300

    Fixed plugin for sylius v1.9

--- src/Builder/Request/ContactBuilder.php
+++ src/Builder/Request/ContactBuilder.php
@@ -57,7 +57,6 @@ class ContactBuilder implements ContactBuilderInterface
             if (!$customer->isSubscribedToNewsletter() && null === $customer->getSubscribedToNewsletterAt()) {
                 $subscribeStatus = ContactIdentifierChannelValue::NON_SUBSCRIBED;
             }
-
             if (!$customer->isSubscribedToNewsletter() && null !== $customer->getSubscribedToNewsletterAt()) {
                 $subscribeStatus = ContactIdentifierChannelValue::UNSUBSCRIBED;
             }
@@ -77,7 +76,6 @@ class ContactBuilder implements ContactBuilderInterface
             if (!$customer->isSubscribedToSmsNewsletter() && null === $customer->getSubscribedToSmsNewsletterAt()) {
                 $phoneSubscribeStatus = ContactIdentifierChannelValue::NON_SUBSCRIBED;
             }
-
             if (!$customer->isSubscribedToSmsNewsletter() && null !== $customer->getSubscribedToSmsNewsletterAt()) {
                 $phoneSubscribeStatus = ContactIdentifierChannelValue::UNSUBSCRIBED;
             }
